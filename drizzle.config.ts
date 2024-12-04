119f338e214bb8cb79db1e3a733464b1281acbf9
Dryd33n <drydenbryson@gmail.com>
2024-12-04 04:19:09 -0800

    downgrade react

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'drizzle-kit';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.NEON_DATABASE_URL) throw new Error('NEON DATABASE_URL not found in environment');
+
+export default {
+  schema: './db/schema.ts',
+  out: './db/migrations',
+ dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEON_DATABASE_URL,
+ },
+  strict: true,
+} satisfies Config
