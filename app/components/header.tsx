2e9102dd5d3a39a13c1af01e51a0270ed4f9f614
Dryd33n <drydenbryson@gmail.com>
2024-12-10 02:20:55 -0800

    convert img to Image

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,23 +1,25 @@
+import Image from 'next/image';
+
 interface HeaderProps {
-    mainText: string;
+  mainText: string;
 }
 
-export default function Header({ mainText }: HeaderProps){
-    return (
-        <header className="relative w-full h-64">
-            <img 
-            src="/header-bg.webp" 
-            alt="Wire Sculpture Background Image"
-            className="absolute inset-0 w-full h-full object-cover" />
-        
-            <div className="absolute inset-0 flex items-center justify-center">
-                <h1 className="text-white lg:text-6xl md:text-5xl sm:text-4xl text-4xl font-extralight tracking-widest">
-                {mainText}
-                </h1>
-            </div>
-        
-        </header>
+export default function Header({ mainText }: HeaderProps) {
+  return (
+    <header className="relative w-full h-64">
+      <Image
+        src="/header-bg.webp"
+        alt="Wire Sculpture Background Image"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+      />
 
-        
-    )
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="text-white lg:text-6xl md:text-5xl sm:text-4xl text-4xl font-extralight tracking-widest">
+          {mainText}
+        </h1>
+      </div>
+    </header>
+  );
 }
