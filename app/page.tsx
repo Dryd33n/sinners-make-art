310ec266e8f43c16679d1d0a7d26df57925e6f90
DrydenD0Texe <drydenbryson@gmail.com>
2024-12-17 18:20:26 -0800

    styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default async function Home() {
   return (<>
     <Header mainText="UNDER CONSTRUCTION"/>
       <div className="flex flex-col items-center justify-center pt-20">
-        <h1 className="text-3xl mx-auto">SINNERS-MAKE.ART IS CURRENTLY UNDER DEVELOPMENT</h1>
-        <p>We are working on something great, please check back later!</p>
+        <h1 className="text-3xl mx-auto text-center">SINNERS-MAKE.ART IS CURRENTLY UNDER DEVELOPMENT</h1>
+        <p className="text-center mt-5">We are working on something great, please check back later!</p>
       </div>
       
     </>
