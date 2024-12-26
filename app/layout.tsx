2cd9d67bc78b29e12a8a1c260b78f3a927c25f7b
Dryd33n <drydenbryson@gmail.com>
2024-12-26 15:35:57 -0800

    black bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} font-sans antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} font-sans antialiased bg-grey-900`}
       >
         {children}
       </body>
