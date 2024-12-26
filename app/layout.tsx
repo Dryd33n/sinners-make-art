8f0c987db3db73b1d62efa0d2e6f9694b48e6615
Dryd33n <drydenbryson@gmail.com>
2024-12-26 15:45:18 -0800

    bg is good

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} font-sans antialiased bg-grey-900`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} font-sans antialiased bg-[#0a0a0a]`}
       >
         {children}
       </body>
