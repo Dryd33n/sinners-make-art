d7b13dea6b45381101280890666103c1c9fc71cf
Dryd33n <drydenbryson@gmail.com>
2024-12-29 01:32:03 -0800

    Update home_button.tsx

--- app/components/home_button.tsx
+++ app/components/home_button.tsx
@@ -1,9 +1,10 @@
+import { index } from "drizzle-orm/mysql-core";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function HomeButton() {
   return (
-<div style={{ position: 'fixed', bottom: 0, left: 0, margin: '10px' }}>
+<div style={{ position: 'fixed', bottom: 0, left: 0, margin: '10px', zIndex: 1000 }}>
   <Link href="/">
   <Image src="/home.png" alt="Home" width={30} height={30}>
   </Image>  
