b522a05b3d441e6400f31485457b6ae6a1c9802d
Dryd33n <drydenbryson@gmail.com>
2024-12-30 17:16:40 -0800

    signature attempt

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import NavBar from './components/global/navBar';
 import Header from './components/global/header';
 import AboutSection from './components/about_section';
 import Footer from './components/global/footer';
+import Signature from './components/global/signature';
 
 export const metadata = {
   title: "Sinners Make Art",
@@ -11,21 +12,9 @@ export const metadata = {
 
 
 const Home = () => {
-  console.log(`%c
-    WEBSITE DESIGNED BY: 
-
-    ██████  ██████  ██    ██ ██████  ███████ ███    ██     ██████     
-    ██   ██ ██   ██  ██  ██  ██   ██ ██      ████   ██     ██   ██    
-    ██   ██ ██████    ████   ██   ██ █████   ██ ██  ██     ██████     
-    ██   ██ ██   ██    ██    ██   ██ ██      ██  ██ ██     ██   ██    
-    ██████  ██   ██    ██    ██████  ███████ ██   ████     ██████  ██ 
-    
-    LinkedIn: https://www.linkedin.com/in/drydenbryson/
-    GitHub:   https://github.com/Dryd33n
-    `,'color: rebeccapurple; font-size: 13px;')
-
   return (
     <>
+      <Signature/>
       <Header mainText="MARY-JANE LARONDE" />
       <NavBar />
 
